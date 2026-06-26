307c62c792473914206ec3c70d21a27232b64d83
Mosesoluwaseye <oluwaseye.moses@stud.th-deg.de>
2026-06-26 11:53:00 +0200

    Fix lib.rs filename

---
+++ tests/planets_kingdoms_tests.rs
@@ -0,0 +1,19 @@
+#[test]
+fn small_graph() {
+    // test code
+}
+
+#[test]
+fn disconnected_graph() {
+    // test code
+}
+
+#[test]
+fn cycle_graph() {
+    // test code
+}
+
+#[test]
+fn compare_algorithms() {
+    // compare Kosaraju and Tarjan outputs
+}
